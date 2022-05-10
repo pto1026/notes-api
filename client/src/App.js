6117bf159a99af5551f3250a9799fe6b0bd56946
Patrick <pto1026@gmail.com>
2022-05-10 15:52:47 -0600

    added notes

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,58 @@
-import Notes from "./components/Notes/Notes";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [notes, setNotes] = useState([]);
+  const [newNote, setNewNote] = useState("");
+  useEffect(() => {
+    axios
+      .get("http://localhost:8000/notes")
+      .then((res) => {
+        setNotes(res.data);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, []);
+  function handleFormSubmit(e) {
+    e.preventDefault();
+
+    axios
+      .post("http://localhost:8000/notes", {
+        text: newNote,
+      })
+      .then((res) => {
+        axios
+          .get("http://localhost:8000/notes")
+          .then((res) => setNotes(res.data))
+          .catch((err) => console.error(err));
+      });
+      setNewNote('');
+  }
   return (
     <div className="App">
       <h1 className="display-1 m-5">Notes App</h1>
-      <button type="button" className="btn btn-primary ms-5">Create new note</button>
-      <Notes className="ms-5"/>
+      <form onSubmit={handleFormSubmit}>
+        <input
+          type="text"
+          value={newNote}
+          onChange={(e) => {
+            setNewNote(e.target.value);
+          }}
+          placeholder="New note"
+        />
+        <button type="submit" className="btn btn-primary m-5">
+          Create new note
+        </button>
+      </form>
+      {/* <Notes className="ms-5"/> */}
+      <ul className="notes">
+        {notes.map((note) => (
+          <li key={note.id} className="note">
+            {note.text}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
